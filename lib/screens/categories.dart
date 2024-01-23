89364a139a361a52cacbcb259c0858c811724915
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-23 21:41:40 +0200

    Filter categories

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_tem.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filterdMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => MealsScreen(
+        title: category.title,
+        meals: filterdMeals,
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +34,10 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
         children: [
-           for (final category in availableCategories) 
-             CategoryGridItem(category: category),
+          for (final category in availableCategories)
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () => _selectCategory(context, category)),
         ],
       ),
     );
