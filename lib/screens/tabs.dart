e1829c985a2ab67d7f418e9e80715d5e0c1bef90
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-24 12:17:08 +0200

    Add bottom navigation tab

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+
+class TabsSCreen extends StatefulWidget {
+   const TabsSCreen({super.key});
+
+  @override
+  State<TabsSCreen> createState() => _TabsSCreenState();
+}
+
+class _TabsSCreenState extends State<TabsSCreen> {
+  int _selectedPageIndex = 0;
+
+  void setSelectedPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage = const MealsScreen(title: 'Favorites', meals: [],);
+      activePageTitle = 'Favorites';
+    }
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: setSelectedPage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
