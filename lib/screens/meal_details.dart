7aad2cdcf5a8a50e79891df839c775a429c8eae5
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-24 11:34:23 +0200

    Add meal details screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -15,50 +15,55 @@ class MealDetailsScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(meal.title),
         ),
-        body: Column(
-          children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            Text(
-              'Ingedients',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 14),
-            for (final ingredient in meal.ingredients)
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
               Text(
-                ingredient,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                )
+                'Ingedients',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 14),
-               Text(
-              'Steps',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
-            ),
-             const SizedBox(height: 14),
-            for (final step in meal.steps)
-              Text(
-                step,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                )
+              for (final ingredient in meal.ingredients)
+                Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )
+                ),
+                const SizedBox(height: 14),
+                 Text(
+                'Steps',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
               ),
-          ],
+               const SizedBox(height: 14),
+              for (final step in meal.steps)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  child: Text(
+                    step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    )
+                  ),
+                ),
+            ],
+          ),
         ));
   }
 }
